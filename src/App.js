41660538a80d438740d8ec5264bbce3e3bfea25c
mandeep83 <mandeep.singh@83incs.com>
2021-02-25 21:46:21 +0530

    USER List

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import UserList from './components/UserList/userList'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Route exact path="/userList"
+          render={props => <UserList {...props} />} />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/UserList/userList.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Table } from 'react-bootstrap'
+
+class UserList extends React.Component {
+	state = {
+		filteredUserList: {
+			data: []
+		},
+		searchTermByField: {
+			first_name: '',
+			last_name: ''
+		}
+	}
+
+	componentDidMount() {
+		this.getUserList()
+	}
+
+	getUserList = () => {
+		fetch("https://reqres.in/api/users").then(res => { return res.json() })
+			.then(response => {
+				this.setState({
+					originalUserList: response,
+					filteredUserList: response
+				})
+			})
+	}
+
+	searchUsers = ({ currentTarget: { id, value } }) => {
+		let searchTermByField = { ...this.state.searchTermByField },
+			filteredUserList = { ...this.state.originalUserList }
+		searchTermByField[id] = value
+
+		if (id == "first_name") {
+			filteredUserList.data = filteredUserList.data.filter(el => el.first_name.toLowerCase().includes(searchTermByField["first_name"].toLowerCase())) //incomplete
+			searchTermByField["last_name"] = ""
+		}
+		else {
+			filteredUserList.data = filteredUserList.data.filter(el => el.last_name.toLowerCase().includes(searchTermByField["last_name"].toLowerCase())) //incomplete
+			searchTermByField["first_name"] = ""
+		}
+		this.setState({
+			searchTermByField
+		})
+	}
+
+	render() {
+		return (
+			<body>
+				<React.Fragment>
+					<div>
+						{this.state.filteredUserList.data.length ?
+							<Table striped bordered style={{ height: "500px" }}>
+								<thead>
+									<tr>
+										{Object.keys(this.state.filteredUserList.data[0]).map((userFields, index) => {
+											return (
+												<th key={index}>
+													{userFields == "first_name" || userFields == "last_name" ? <React.Fragment> {userFields} < input type="text" id={userFields} value={this.state.searchTermByField[userFields]} onChange={this.searchUsers} /></React.Fragment> : userFields}
+												</th>)
+										})}
+									</tr>
+								</thead>
+								<tbody>
+									{this.state.filteredUserList.data.map(user => {
+										return (
+											< tr key={user.id} >
+												{
+													Object.keys(this.state.filteredUserList.data[0]).map((userFields, index) => {
+														return (<td key={index}>
+															{userFields != "avatar" ? user[userFields] : <img src={user[userFields]} />}
+														</td>)
+													})
+												}
+											</tr>)
+									})}
+								</tbody>
+							</Table> : null}
+
+					</div>
+				</React.Fragment >
+			</body >
+		)
+	}
+}
+
+export default UserList
